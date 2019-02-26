8fc9ca00bcf6a02463e74e84a6fa023d7562aea4
dongecs <dongecs@qq.com>
2019-02-26 18:19:24 +0800

    修改结构

--- template/src/main/java/com/dsa/array/ArrayApp.java
+++ template/src/main/java/com/dsa/array/ArrayApp.java
@@ -19,47 +19,8 @@ public class ArrayApp {
         statisArray.remove(1);
     }
 
-    /**
-     * 合并有序数组(升序)
-     * @param arr1 有序数组
-     * @param arr2 有序数组
-     * @return
-     */
-    public static int[] mergeArrs(int[] arr1, int[] arr2){
-        int mgIndex = 0, index1 = 0, index2 = 0;
-        int totalLen = arr1.length + arr2.length;
-        int[] mergeArr = new int[totalLen];
-        while (mgIndex < totalLen) {
-            if (arr1[index1] < arr2[index2]) {
-                mergeArr[mgIndex++] = arr1[index1++];
-            } else {
-                mergeArr[mgIndex++] = arr2[index2++];
-            }
-            if (index1 == arr1.length) {
-                for (int i = index2; i < arr2.length; ++i) {
-                    mergeArr[mgIndex++] = arr2[i];
-                }
-            } else if (index2 == arr2.length) {
-                for (int i = index1; i < arr1.length; ++i) {
-                    mergeArr[mgIndex++] = arr1[i];
-                }
-            }
-        }
-        return mergeArr;
-    }
-
-    private static void testMergeData() {
-        int[] arr1 = {1, 4, 7, 10, 11, 16, 17};
-        int[] arr2 = {0, 1, 5, 8, 10};
-        int[] mergeArrs = mergeArrs(arr1, arr2);
-        for (int arrNum : mergeArrs) {
-            System.out.print(arrNum + " ");
-        }
-    }
-
     public static void main(String[] args) {
 //        dynamicApp();
 //        statisApp();
-        testMergeData();
     }
 }
--- template/src/main/java/com/dsa/array/StatisArray.java
+++ template/src/main/java/com/dsa/array/StatisArray.java
@@ -65,4 +65,33 @@ public class StatisArray {
     public int getSize() {
         return size;
     }
+
+    /**
+     * 合并有序数组(升序)
+     * @param arr1 有序数组
+     * @param arr2 有序数组
+     * @return
+     */
+    public static int[] mergeArrs(int[] arr1, int[] arr2){
+        int mgIndex = 0, index1 = 0, index2 = 0;
+        int totalLen = arr1.length + arr2.length;
+        int[] mergeArr = new int[totalLen];
+        while (mgIndex < totalLen) {
+            if (arr1[index1] < arr2[index2]) {
+                mergeArr[mgIndex++] = arr1[index1++];
+            } else {
+                mergeArr[mgIndex++] = arr2[index2++];
+            }
+            if (index1 == arr1.length) {
+                for (int i = index2; i < arr2.length; ++i) {
+                    mergeArr[mgIndex++] = arr2[i];
+                }
+            } else if (index2 == arr2.length) {
+                for (int i = index1; i < arr1.length; ++i) {
+                    mergeArr[mgIndex++] = arr1[i];
+                }
+            }
+        }
+        return mergeArr;
+    }
 }
